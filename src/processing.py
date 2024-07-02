0dca88b9a7508053b951f35d4002b0606eb02fad
rostislav <rostislavkenobi@outlook.com>
2024-07-02 21:17:29 +0300

    добавлена функция которая возвращает список словарей отсортированый по убыванию даты

--- src/processing.py
+++ src/processing.py
@@ -15,3 +15,15 @@ def filter_by_state(list_of_dict: list[dict[str, Any]], state: str = "EXECUTED")
     значение.
     """
     return [d for d in list_of_dict if d.get("state") == state]
+
+def sort_by_date(
+    list_of_dict: list[dict[str, Any]], reverse: bool = True
+) -> list[dict[str, Any]]:
+    """
+    Функция принимает на вход список словарей и возвращает новый список, в котором исходные
+    словари отсортированы по убыванию даты
+    """
+    sorted_list = sorted(
+        list_of_dict, key=lambda new_list_of_dict: new_list_of_dict["date"], reverse=reverse
+    )
+    return sorted_list
